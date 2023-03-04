dee27af30f0c9ad8c6e4fc8a2077c14b48c1455a
Pavel Kononov <kononoffp@gmail.com>
2023-03-03 20:51:15 -0500

    Mongo init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule]
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    UsersModule
+  ]
 })
+
 export class AppModule {}
