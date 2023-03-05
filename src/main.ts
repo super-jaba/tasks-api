73c85d3b67e60f98c9d4fa3b65887efb5f4ad956
Pavel Kononov <kononoffp@gmail.com>
2023-03-04 20:02:27 -0500

    Swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tasks API')
+    .setDescription('Docs')
+    .setVersion('1.0.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
 
   // TODO: Make a func to get port
   await app.listen(3000);
